0c1679d1cd81dc773241571e0bec81aad79214e2
Peterj215 <peterj215@hotmail.com>
2017-06-18 16:56:03 -0400

    Holocaption w/ drag & color change

---
+++ holocaption-peter/Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+    public GameObject cube;
+    public void Slider_Changed(float newValue)
+    {
+        Vector3 pos = cube.transform.position;
+        pos.z = newValue;
+        cube.transform.position = pos;
+    }
+
+    }
+	/*
+    public void MakeItGreen ()
+    {
+        GetComponent<Text>().color = Color.green;
+
+    }
+
+    public void MakeItBlue ()
+    {
+        GetComponent<Text>().color = Color.blue;
+    }
+
+
+}*/
